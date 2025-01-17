26f6d1b9e03507066956d46072f3681a9c739f8a
Lindsey Bell <lindseybell@snrenet.arizona.edu>
2025-01-17 10:09:20 -0700

    [major] fixed zscore sums and added water year designations; seasonal totals are too high, however and need to be fixed

--- Code/FindSeasonalPrecipPattern.R
+++ Code/FindSeasonalPrecipPattern.R
@@ -1,12 +1,10 @@
-#basic stats to cluster DOY rainfall for sum and win seasons
-#working w dummy numbers rn ^
-#would be better to sum rainfall through DOY for each year and each season rather than average it
+#currently static dates for seasons, but will implement break point analysis on rainfall accumulation curve
 
-#Script to build a dataframe of seasonal precip anomalies for a flux site
+#Script to build a data frame of seasonal precip anomalies for a flux site
   # 1. identify date and precip column
-  # 2. bin precip by winter and summer seasons using some sort of natural breaks stat
+  # 2. bin precip by winter and summer seasons 
   # 3. calculate summer and winter precip zscores for each year
-  # 4. denote positive values as wet and negaive values as dry
+  # 4. denote positive values as wet and negative values as dry
   # 5. clean final df
 
 #===============================================================================
@@ -28,40 +26,45 @@ columns_of_interest <- flux_data %>%
             Day = day(TIMESTAMP_START),
             Day_of_Year = yday(TIMESTAMP_START))%>%
   group_by(Year, Month, Day)%>%
-  mutate(Precip = mean(P, na.rm = T))%>% #may need to change to P_PI
-  select(Year, Month, Day, Day_of_Year, Precip)
+  mutate(Precip = sum(P, na.rm = T))%>% #may need to change to P_PI
+  select(Year, Month, Day, Day_of_Year, Precip) #note precip is in mm
 
 #plot(columns_of_interest$Day_of_Year, columns_of_interest$Precip)
 
 #===============================================================================
-#Identify Summer Rainy Season Dates and Winter Rainy Season Dates
+#Label Water Years and Define Winter/Summer Seasons (Static Approach)
 #===============================================================================
-DayOfYearPrecip <- columns_of_interest%>%
-  group_by(Day_of_Year)%>%
-  summarize(AveragePrecip = mean(Precip, na.rm = T))
-DayOfYearPrecip <- DayOfYearPrecip%>%
-  mutate(sinDOY = sin(2*pi*Day_of_Year/366),
-         cosDOY = cos(2*pi*Day_of_Year/366)) #transform to sin/cos so dates are continuous (ie don't start/stop at 1/366)
+#DayOfYearPrecip <- columns_of_interest%>%
+ # group_by(Day_of_Year)%>%
+  #summarize(AveragePrecip = mean(Precip, na.rm = T))
+#DayOfYearPrecip <- DayOfYearPrecip%>%
+ # mutate(sinDOY = sin(2*pi*Day_of_Year/366),
+        # cosDOY = cos(2*pi*Day_of_Year/366)) #transform to sin/cos so dates are continuous (ie don't start/stop at 1/366)
+water_year <- columns_of_interest%>%
+  mutate(water_year = ifelse(Month > 9, Year + 1, Year))
 
+SeasonsDOY <- water_year%>%
+  mutate(Season = case_when(Day_of_Year >=166 & Day_of_Year <= 288 ~ "Summer", Day_of_Year >=305 | Day_of_Year <= 59 ~ "Winter", T ~ "DrySeason"))%>%
+  mutate(sinDOY = sin(2*pi*Day_of_Year/366),
+         cosDOY = cos(2*pi*Day_of_Year/366))
+
+WinSumPpt <- SeasonsDOY%>%
+  filter(Season != "DrySeason")
 #===============================================================================
 #Calculate Seasonal Z-Scores
 #===============================================================================
-#dummy values: summer from DOY 165-250, winter from 340-90   
-SeasonsDOY <- columns_of_interest%>%
-  mutate(Season = case_when(Day_of_Year >=165 & Day_of_Year <= 250 ~ "Summer", T ~ "Winter"))%>%
-  mutate(sinDOY = sin(2*pi*Day_of_Year/366),
-         cosDOY = cos(2*pi*Day_of_Year/366))
-AvgPrecip <- SeasonsDOY%>%
-  group_by(Year, Season)%>%
-  summarize(AnnualPrecip = mean(Precip, na.rm = T))
-longterm_values <- SeasonsDOY%>%
+AvgPrecip <- WinSumPpt%>%
+  group_by(water_year, Season)%>%
+  summarize(TotalAnnualPrecip = sum(Precip, na.rm = T))
+
+longterm_values <- AvgPrecip%>%
   group_by(Season)%>%
-  summarize(longterm_avg = mean(Precip, na.rm = T),
-         longterm_sd = sd(Precip, na.rm = T))
+  summarize(longterm_avg = mean(TotalAnnualPrecip, na.rm = T),
+         longterm_sd = sd(TotalAnnualPrecip, na.rm = T))
 calc_df <- merge(AvgPrecip, longterm_values, by = "Season")
 zscores <- calc_df%>%
-  group_by(Year, Season)%>%
-  summarize(zscore = (AnnualPrecip - longterm_avg)/longterm_sd)
+  group_by(water_year, Season)%>%
+  summarize(zscore = (TotalAnnualPrecip - longterm_avg)/longterm_sd)
 
 #===============================================================================
 #Clean Up Dataframe
@@ -69,11 +72,12 @@ zscores <- calc_df%>%
 result_df <- zscores%>%
   pivot_wider(names_from = Season, 
               values_from = zscore)%>%
-  select(Year, Winter, Summer)
+  select(water_year, Winter, Summer)
 
 result_df <- result_df%>%
   rename(SummerPrecipZscore = Summer, 
-         WinterPrecipZscore = Winter)%>%
+         WinterPrecipZscore = Winter,
+         WaterYR = water_year)%>%
   mutate(PrecipCondition = case_when(
   WinterPrecipZscore < 0 & SummerPrecipZscore < 0 ~ "DD",
   WinterPrecipZscore < 0 & SummerPrecipZscore > 0 ~ "DW",
