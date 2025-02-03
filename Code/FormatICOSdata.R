07be91ef167f73e4e60c198c5c9429c42e31db2b
Lindsey Bell <lindseybell@snrenet.arizona.edu>
2025-02-03 16:44:36 -0700

    ICOS flux df made (w IGBP)

--- Code/FormatICOSdata.R
+++ Code/FormatICOSdata.R
@@ -1,43 +1,51 @@
+#extract flux data from zip files and store in R obj
+#===============================================================================
+# Load Necessary Packages
+#===============================================================================
 library(zip)
 library(readr)
 library(lubridate)
 library(dplyr)
 library(stringr)
+#===============================================================================
+# Extract Half-Hourly Data from Zip Folders on Snow X Drive
+#===============================================================================
+ICOSdatapath <- "X:/moore/ICOSdata"
 
-#extract flux data from zip files and store in R obj
-folder_path <- "X:/moore/ICOSdata"
-zip_files <- list.files(folder_path, pattern = "FLUXNET_HH_L2\\.zip$", full.names = TRUE)
-read_fluxnet_csv <- function(zip_file) {
-  files_in_zip <- zip::zip_list(zip_file)$filename
+zip_files <- list.files(ICOSdatapath, pattern = "FLUXNET_HH_L2\\.zip$", full.names = T)
+fluxnet_data <- lapply(zip_files, function(i) {
+  files_in_zip <- zip::zip_list(i)$filename
   csv_file <- files_in_zip[grepl("\\.csv$", files_in_zip)]
-  if (length(csv_file) == 1) {
-    con <- unz(zip_file, csv_file)
-    df <- read_csv(con)
-    return(df)
-  } else {
-    warning(paste("No CSV found in:", zip_file))
-    return(NULL)
-  }
-}
-fluxnet_data <- lapply(zip_files, read_fluxnet_csv)
+  
+  con <- unz(i, csv_file)
+  df <- read_csv(con)
+  return(df)
+})
+
+#extract site code and add to dfs
 extract_site_code <- function(filename) {
   str_extract(filename, "[A-Z]{2}-[A-Za-z]{3}")
 }
+fluxnet_data <- setNames(fluxnet_data, basename(zip_files))
+#sites without HH FluxNet product
+removal_site <- c("DE-BeR", "FI-Kmp", "FI-Kur", "FI-Tum", "GR-HeK", "GR-Hem", "IT-BFt")
 fluxnet_data <- lapply(names(fluxnet_data), function(name) {
   df <- fluxnet_data[[name]]
   site_code <- extract_site_code(name)
   
-  if (!is.null(df)) {
+  if (!is.null(df) && !site_code %in% removal_site) {
     df <- df %>% mutate(Site_Code = site_code)
+    return(df)
+  } else {
+    return(NULL)
   }
-  
-  return(df)
 })
-names(fluxnet_data) <- basename(zip_files)
+
 #saveRDS(fluxnet_data, "./Data/ICOSlist.RDS")
+#format like Ameriflux to combine dfs
 flux_coi <- lapply(fluxnet_data, function(i){
   i %>%
-    select(TIMESTAMP_START, GPP_DT_VUT_75) %>%
+    select(Site_Code, TIMESTAMP_START, GPP_DT_VUT_75) %>%
     mutate(TIMESTAMP_START = ymd_hm(TIMESTAMP_START),
            Year = year(TIMESTAMP_START),
            DOY = yday(TIMESTAMP_START))%>%
@@ -45,30 +53,34 @@ flux_coi <- lapply(fluxnet_data, function(i){
     mutate(dailyGPP = mean(GPP_DT_VUT_75, na.rm = T))%>%
     ungroup()%>%
     group_by(Year)%>%
-    summarize(TotalGPP = sum(GPP_DT_VUT_75, na.rm = T)) %>%
+    summarize(TotalGPP = sum(GPP_DT_VUT_75, na.rm = T),
+              Site_Code = Site_Code) %>%
+    ungroup()%>%
+    distinct()%>%
     mutate(GPPLongtermAvg = mean(TotalGPP, na.rm = T),
            GPPLongtermSD = sd(TotalGPP, na.rm = T),
-           GPPZScore = (TotalGPP - GPPLongtermAvg) / GPPLongtermSD)
+           GPPZScore = (TotalGPP - GPPLongtermAvg) / GPPLongtermSD)%>%
+    rename(SiteID = Site_Code)
 })
 
-removal_site <-  c("DE-BeR", "FI-Kmp", "FI-Kur", "FI-Tum", "GR-HeK", "GR-Hem", "IT-BFt")
-#Add ICOS data to FluxNet data
+fluxdf <- bind_rows(flux_coi)
+#===============================================================================
+#Pull IGBP from Archive folders
 #===============================================================================
 #extract IGBP from SiteInfo in Archive folders
 zip_files <- list.files(folder_path, pattern = "ARCHIVE_L2\\.zip$", full.names = TRUE)
-read_archive_csv <- function(zip_file) {
-  files_in_zip <- zip::zip_list(zip_file)$filename
-  csv_file <- files_in_zip[grepl("SITEINFO_L2.csv$", files_in_zip)]
-  if (length(csv_file) == 1) {
-    con <- unz(zip_file, csv_file)
-    df <- read_csv(con)
-    return(df)
-  } else {
-    warning(paste("No CSV found in:", zip_file))
-    return(NULL)
-  }
+extract_site_code <- function(filename) {
+  str_extract(filename, "[A-Z]{2}-[A-Za-z]{3}")
 }
-archive_data <- lapply(zip_files, read_archive_csv)
+removal_site <- c("DE-BeR", "FI-Kmp", "FI-Kur", "FI-Tum", "GR-HeK", "GR-Hem", "IT-BFt")
+zip_files <- zip_files[!extract_site_code(basename(zip_files)) %in% removal_site]
+archive_data <- lapply(zip_files, function(i) {
+  files_in_zip <- zip_list(i)$filename
+  csv_file <- files_in_zip[grepl("SITEINFO_L2.csv$", files_in_zip)]
+  con <- unz(i, csv_file)
+  df <- read_csv(con)
+    return(df)
+  })
 names(archive_data) <- basename(zip_files)
 
 arch_df <- bind_rows(archive_data)%>%
@@ -76,3 +88,9 @@ arch_df <- bind_rows(archive_data)%>%
   select(SITE_ID, DATAVALUE)%>%
   rename(SiteID = SITE_ID,
          IGBP_PI = DATAVALUE)
+
+ICOSdf <- merge(fluxdf, arch_df, by = "SiteID")
+
+ggplot(ICOSdf, aes(x = Year, y = GPPZScore, color = IGBP_PI))+
+  geom_point()+
+  theme_minimal()
